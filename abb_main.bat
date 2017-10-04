2bd037740cc0ac378e5df725c13aa11443997b53
ssaczkows <ssaczkows@outlook.com>
2017-10-03 23:33:02 -0300

    Elimino archivos innecesarios

--- abb_main.bat
+++
@@ -1,5 +0,0 @@
-nasm -f win32 -o _agregar_abb.o _agregar_abb.asm
-nasm -f win32 -o _mostrar_abb.o _mostrar_abb.asm
-mingw32-gcc -Wall -c abb_main.c -o abb_main.o
-mingw32-gcc -Wall -o main abb_main.o _agregar_abb.o _mostrar_abb.o 
-main.exe
