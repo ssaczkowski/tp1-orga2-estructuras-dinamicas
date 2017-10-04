2dc3189affae7fcd76dc286b6ae162cd7393f0c7
ssaczkows <ssaczkows@outlook.com>
2017-10-04 00:35:17 -0300

    Actualizo archivos

--- _borrar_abb.asm
+++ _borrar_abb.asm
@@ -1,20 +1,11 @@
-;  ja mayor         **POSIBLES RESULTADOS
-;  jb menor
-;  je igual     
+    
 section .data
-msg:    db     `\nPARTE IZQ\n`, 10, 0
-msg1:    db    "PARTE DER", 10, 0
-msg3:    db    "PUNTERO %p", 10, 0
-msg4: db    "ENTRA A LA FUNCION CON PUNTERO CERO (NULL)", 10, 0
-msgD: db `\nVALORES A COMPARAR\nvalor: %d y contenido: %p`, 10, 0
-msg6:    db    "VALOR INSERTADO EN PUNT Izq  %d", 10, 0
-msg7:    db    "VALOR INSERTADO EN PUNT DER  %d", 10, 0
-prueba:    db     `***VALOR EN EL NODO %p\n`, 10, 0
-msjPV: db  `****VALOR : %p Y PUNTERO : %p****\n`, 10, 0
-msjPVV: db  `VALORES BORRADOS\n****VALOR : %p Y PUNTERO : %p****\n`, 10, 0
-msjP: db  `****VALOR : %p Y valor : %p****\n`, 10, 0
-msjMe: db `*****RAIZ MENOR*****\n`, 10, 0
-msjMa: db `*****RAIZ MAYOR*****\n`, 10, 0
+
+msg_verPunteroValor: db  `****VALOR : %p Y PUNTERO : %p****\n`, 10, 0
+msg_verValoresBorrados: db  `VALORES BORRADOS\n****VALOR : %p Y PUNTERO : %p****\n`, 10, 0
+msg_punteros: db  `****VALOR : %p Y valor : %p****\n`, 10, 0
+msg_menor: db `*****RAIZ MENOR*****\n`, 10, 0
+msg_mayor: db `*****RAIZ MAYOR*****\n`, 10, 0
 
 section .text
 extern _borraNodo
@@ -38,7 +29,7 @@ _borrar_abb:
     mov eax,[ebx] ;PASO EL CONTENIDO DEL NODO A BORRAR
     push dword edi
     push dword eax
-    push dword msjP
+    push dword msg_punteros
     call _printf
     add esp,4
     pop eax
@@ -49,96 +40,90 @@ _borrar_abb:
     jb raizMenor
     
     raizMayor:
-    PUSH DWORD msjMa
-    call _printf
-    add esp,4
+        PUSH DWORD msg_mayor
+        call _printf
+        add esp,4
     
-     mov EDX, [EBP+8] ;PUNTERO AL ARBOL
-     mov EBX, [EBP+12];PUNTERO A BORRAR
-     mov edx,[edx]
-     add edx,8
-    ; mov edx,[edx]
-     ;
-     push dword edx
-     push dword ebx
-     push dword msjPV
-     call _printf
-     add esp,4
-     pop ebx
-     pop edx
-        ;
+        mov EDX, [EBP+8] ;PUNTERO AL ARBOL
+        mov EBX, [EBP+12];PUNTERO A BORRAR
+        mov edx,[edx]
+        add edx,8
+    
+        push dword edx
+        push dword ebx
+        push dword msg_verPunteroValor
+        call _printf
+        add esp,4
+        pop ebx
+        pop edx
+        
      
-     push EBX
-     push edx
-     call _borrar_abb
-     pop edx
-     pop ebx ;add ESP, 8
-     jmp fin
+        push EBX
+        push edx
+        call _borrar_abb
+        pop edx
+        pop ebx 
+        jmp fin
      
       raizMenor:
-      PUSH DWORD msjMe
-      call _printf
-      add esp,4
+         push DWORD msg_menor
+         call _printf
+         add esp,4
       
-      mov EDX, [EBP+8] ;PUNTERO AL ARBOL
-     mov EBX, [EBP+12];PUNTERO A BORRAR
-     mov edx,[edx]
-     add edx,4
-    ; mov edx,[edx]
-     ;
-     push dword edx
-     push dword ebx
-     push dword msjPV
-     call _printf
-     add esp,4
-     pop ebx
-     pop edx
-        ;
+         mov EDX, [EBP+8] ;PUNTERO AL ARBOL
+         mov EBX, [EBP+12];PUNTERO A BORRAR
+         mov edx,[edx]
+         add edx,4
+    
+    
+         push dword edx
+         push dword ebx
+         push dword msg_verPunteroValor
+         call _printf
+         add esp,4
+         pop ebx
+         pop edx
+        
      
-     push EBX
-     push edx
-     call _borrar_abb
-     pop edx
-     pop ebx ;add ESP, 8
-     jmp fin
+         push EBX
+         push edx
+         call _borrar_abb
+         pop edx
+         pop ebx 
+         jmp fin
     
     
     borrar:
- ;   mov ebp, esp; for correct debugging                                 
-  ;  push EBP
-  ;  mov EBP, ESP
-    push dword esi
-    push dword ebx
-    push dword msjPVV
-    call _printf
-    add esp,4
-    pop ebx
-    pop esi
+
+        push dword esi
+        push dword ebx
+        push dword msg_verValoresBorrados
+        call _printf
+        add esp,4
+        pop ebx
+        pop esi
     
-    ; 
-     mov EDX, [EBP+8] ;PUNTERO AL ARBOL 
-     push dword edx
-     push dword esi
-     push dword msjP
-     call _printf
-     add esp,4
-     pop esi
-     pop edx
-    ;
-    mov esi,0
-    mov [edx],esi
+    
+        mov EDX, [EBP+8] ;PUNTERO AL ARBOL 
+        push dword edx
+        push dword esi
+        push dword msg_punteros
+        call _printf
+        add esp,4
+        pop esi
+        pop edx
+    
+        mov esi,0
+        mov [edx],esi
      
-    mov ebp, esp; for correct debugging                                 
-    push EBP
-    mov EBP, ESP
-    push ebx
-    call _borraNodo
-    pop ebx
-    pop ebp
- ;   pop ebx
-  ;  pop EBP
-  ;  mov ebx,0
-  ;  mov [edx],ebx
+        mov ebp, esp                                
+        push EBP
+        mov EBP, ESP
+        push ebx
+        call _borraNodo
+        pop ebx
+        pop ebp
+    
       
     fin: 
      mov     ESP, EBP
--- _mostrar_abb.asm
+++ _mostrar_abb.asm
@@ -4,9 +4,10 @@ extern _printf
 global _mostrar_abb
 
 section .data
-msg1:    db "IMPRIMO ARBOL BINARIO EN PRE-ORDEN", 10, 0
-msg2:    db `%d`,10,0
-NULL equ 0
+
+msg_valor:    db `%d`,10,0
+
+
 section .text
 
 
@@ -18,50 +19,47 @@ _mostrar_abb:
     mov esi,edi
     mov ebx,esi    
      
-    cmp ebx, 0 ;HAY QUE COMPARAR SI EL PUNTERO RECIBIDO ES NULL
+    cmp ebx, 0 
     je  finalizar
-  ;  jmp seguir      ;EL PUNTERO NO ES NULL, POR LO TANTO IMPRIMO
-    
-  ;  seguir:   
-              ;IMPRIMO VALOR
-        mov ebx,[ebx] ;COMO EL PUNTERO RECIBIDO NO ES NULL, TENEMOS QUE RECUPERAR EL CONTENIDO.
-        push ebx
-        push msg2
-        call _printf
-        add ESP, 4
-        pop ebx
+      
+    ; Para test: IMPRIMO VALOR
+    mov ebx,[ebx] 
+    push ebx
+    push msg_valor
+    call _printf
+    add ESP, 4
+    pop ebx
         
-        ;IMPRIMO RECURSIVAMENTE SUBARBOL IZQ
+    ;IMPRIMO RECURSIVAMENTE SUBARBOL IZQ
       
         
-        add edi,8  
-        mov edi,[edi]                           
-        push EBP
-        mov EBP, ESP
-        push edi
-        call _mostrar_abb
-        pop edi
-        pop ebp 
+    add edi,8  
+    mov edi,[edi]                           
+    push EBP
+    mov EBP, ESP
+    push edi
+    call _mostrar_abb
+    pop edi
+    pop ebp 
      
      
         
-        ;IMPRIMO RECURSIVAMENTE SUBARBOL DER
-         mov EDI,[EBP+8] ;DIRECCION DEL PUNTERO
-        ADD edi,4  
-        mov edi,[edi]                           
-        push EBP
-        mov EBP, ESP
-        push edi
-        call _mostrar_abb
-        pop edi
-        pop ebp 
+    ;IMPRIMO RECURSIVAMENTE SUBARBOL DER
+    mov EDI,[EBP+8] ;DIRECCION DEL PUNTERO
+    ADD edi,4  
+    mov edi,[edi]                           
+    push EBP
+    mov EBP, ESP
+    push edi
+    call _mostrar_abb
+    pop edi
+    pop ebp 
      
         
 
    finalizar:
    
-        mov     ESP, EBP
-        pop     EBP
-        ret  
-       
+    mov  ESP, EBP
+    pop  EBP
+    ret    
    
