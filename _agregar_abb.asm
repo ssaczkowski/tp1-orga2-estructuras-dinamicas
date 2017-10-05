4f80e365ff6a9929bcf2b702b369146d142dabcb
ssaczkows <ssaczkows@outlook.com>
2017-10-05 02:24:17 -0300

    Refactor para mejorar legibilidad

--- _agregar_abb.asm
+++ _agregar_abb.asm
@@ -1,17 +1,12 @@
-  ;  ja mayor         **POSIBLES RESULTADOS
-;  jb menor
-;  je igual     
+   
 section .data
-msgB:    db     `\n**************INICIO DE INSERTAR\n`, 10, 0
-msg:    db     `\nPARTE IZQ\n`, 10, 0
-msg1:    db    "PARTE DER", 10, 0
-msg3:    db    "PUNTERO %p", 10, 0
-msg4: db    "ENTRA A LA FUNCION CON PUNTERO CERO (NULL)", 10, 0
-msgD: db `\nVALORES A COMPARAR\nvalor: %d y contenido: %p`, 10, 0
-msg6:    db    "VALOR INSERTADO EN PUNT Izq  %d", 10, 0
-msg7:    db    "VALOR INSERTADO EN PUNT DER  %d", 10, 0
-prueba:    db     `***VALOR EN EL NODO %p\n`, 10, 0
-msjPV: db  `****VALOR : %d Y PUNTERO : %p****\n`, 10, 0
+
+msg_insertar:    db     `\n**************INICIO DE INSERTAR\n`, 10, 0
+msg_parteIZQ:    db     `\nPARTE IZQ\n`, 10, 0
+msg_parteDER:    db    "PARTE DER", 10, 0
+msg_puntero:    db    "PUNTERO %p", 10, 0
+msg_valoresAComparar: db `\nVALORES A COMPARAR\nvalor: %d y contenido: %p`, 10, 0
+msg_punteroYValor: db  `****VALOR : %d Y PUNTERO : %p****\n`, 10, 0
 
 section .text
 extern _malloc
@@ -19,181 +14,105 @@ extern _printf
 global _insertaNodo
 
 _insertaNodo:
-    push dword msgB
+    push dword msg_insertar
     call _printf
     add esp,4
-
     push EBP
     mov EBP, ESP
     mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
     mov EBX, [EBP+12];VALOR RECIBIDO
     mov esi,[EDX] ;CONTENIDO DEL PUNTERO EN VALOR      
-     ;   
-        push dword edx
-        push dword [edx]
-        push dword msjPV
-        call _printf
-        add esp,4
-        pop edx
-     ;     
+    push dword edx
+    push dword [edx]
+    push dword msg_punteroYValor
+    call _printf
+    add esp,4
+    pop edx
     mov edi,0
     cmp edi,esi
-    je      igual
-    jmp     noIgual
-    igual:      
-        mov	eax, 12                    ; size of the structure
+    je      punteroEsNull
+    jmp     punteroNoNull
+    
+    
+    punteroEsNull:      
+        mov	eax, 12                   
         push    eax
-        call    _malloc                     ; get the memory allocated
+        call    _malloc                   
         mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
-        mov    [edx],eax ;EN EL CONTENIDO DEL PUNTERO LE ASIGNO LA DIRECCION QUE DEVUELVE MALLOC
-        ;push dword [edx]
-        ;push dword eax
-        ;push dword msgP
-        ;call _printf
-        ;add esp,4
-        ;pop eax
-        ;pop edx
+        mov    [edx],eax ;EN EL CONTENIDO DEL PUNTERO LE ASIGNO LA DIRECCION QUE DEVUELVE MALLOC   
         mov     edx,[edx]
         mov     [edx],ebx
         mov     ebx,0
         mov     [edx+4],ebx
         mov     [edx+8],ebx
-    
         add      ESP, 4
         jmp     finalizar
         
        
-          
-    noIgual:
-    ;EBX TIENE EL SIGUIENTE VALOR INGRESADO
-    ;EDX TIENE LA DIRECCION DE MEMORIA DEL NODO ANTERIOR
-    ;[EDX] TIENE EL VALOR DEL NODO DEL VALOR ANTERIOR
-    ;MOSTRAR EL VALOR DEL NODO ANTERIOR
-    ;mov esi,[edx]
-    ;push dword esi ; 
-    ;push dword msg3
-    ;call _printf
-        ;
-        ;COMPARO SI EL VALOR INGRESADO ES MENOR AL VALOR DEL NODO ANTERIOR
+    punteroNoNull:
+         mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
+         mov EBX, [EBP+12];VALOR RECIBIDO
+         mov esi,[EDX] ;CONTENIDO DEL PUNTERO EN VALOR
+         push dword [edx]
+         push dword ebx
+         push dword msg_valoresAComparar
+         call _printf
+         add esp,4
+         pop ebx
+         pop edx
+         cmp ESI,EBX
+         jb  seteoElPtrDerDelNodo
+         ja seteoElPtrIzqDelNodo
+         jmp existe
     
-      
-     mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
-     mov EBX, [EBP+12];VALOR RECIBIDO
-     mov esi,[EDX] ;CONTENIDO DEL PUNTERO EN VALOR
-      ; 
-        
-        push dword [edx]
-        push dword ebx
-        push dword msgD
-        call _printf
-        add esp,4
-        pop ebx
-        pop edx
-     ;   
-      cmp ESI,EBX
-      
-      jb  seteoElPtrDerDelNodo
-      ja seteoElPtrIzqDelNodo
-      jmp existe
-     ;COMPARO SI EL VALOR INGRESADO ES MAYOR AL VALOR DEL NODO ANTERIOR
-     ;cmp ESI,ebx
      
     existe:
-    
-    mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
-    mov eax,edx
-    jmp finalizar
+        mov EDX, [EBP+8] ;DIRECCION DEL PUNTERO
+        mov eax,edx
+        jmp finalizar
     
      seteoElPtrDerDelNodo:
-     push dword msg1 ; ***MOSTRAR QUE ENTRA A LA IZQ, EL VALOR INGRESADO ES MENOR AL QUE ESTA EN EL NODO***
-     call _printf
-     add  ESP, 4
-    
-       mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
-    ;  mov esi,edx ;AHORA ESI TIENE LA DIRECCION DEL PUNTERO NODO 
-      add esi,4 ; AUMENTAMOS 4 A EDX PARA ACCEDER A LA DIRECCION DEL PUNTERO DEL NODO DER
-;      mov esi,[esi]
-     
-          ;  push EBP
-        ;  mov EBP, ESP
-        mov edx,esi
-     mov edx,[edx]
-        cmp edx,0
-     je llamo
-     mov esi,edx 
-     llamo:
-      push ebx
-      push esi
-      call _insertaNodo
-      pop esi
-      pop ebx
-    ;  pop ebp
-   ;   mov ebx,0
-   ;   cmp [esi],ebx
-   ;   je asignar
-    ;  mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
-    ;  mov esi,edx ;AHORA ESI TIENE LA DIRECCION DEL PUNTERO NODO 
-    ;  add esi,4 ; AUMENTAMOS 4 A EDX PARA ACCEDER A LA DIRECCION DEL PUNTERO DEL NODO DER
-        ;  mov esi,[esi]
-       ; mov esi,[esi]
-        
-  
-    jmp finalizar
-   ; asignar:
-   ; mov [esi],eax
-   ; jmp finalizar
+         push dword msg_parteDER
+         call _printf
+         add  ESP, 4
+         mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
+         add esi,4 
+         mov edx,esi
+         mov edx,[edx]
+         cmp edx,0
+         je insertarElNodo1
+         mov esi,edx 
+     insertarElNodo1:
+          push ebx
+          push esi
+          call _insertaNodo
+          pop esi
+          pop ebx
+          jmp finalizar
+ 
     
     seteoElPtrIzqDelNodo:
-    
-   ; push dword ebx ;   ++++MOSTRAR VALORES PARA PRUEBA+++++++
-   ; push dword esi ***MOSTRAR QUE ENTRA A LA DER, EL VALOR INGRESADO ES MAYOR AL QUE ESTA EN EL NODO***
-    push dword msg
-    call _printf
-    add   ESP, 4
-    
-    mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
-;   mov esi,edx ;AHORA ESI TIENE LA DIRECCION DEL PUNTERO NODO 
-    add esi,8 ; AUMENTAMOS 8 A EDX PARA ACCEDER A LA DIRECCION DEL PUNTERO DEL NODO IZQ
-    mov edx,esi
-    mov edx,[edx]
-    cmp edx,0
-    je llamar
-    mov esi,edx  
-  ;  mov esi,[esi]
-  ;  mov esi,[esi]
-    ;push dword esi ;******LINEAS PARA IMPRIMIR LA DIRECCION POR PANTALLA****
-    ;push dword msg3
-    ;call _printf
-    ;add esp,8
-    ;******LLAMAR RECURSIVAMENTE****
-   ;   push EBP
-   ;   mov EBP, ESP
-   llamar:
-      push ebx
-      push esi
-      call _insertaNodo
-      pop esi
-      pop ebx
-   ;   pop ebp
-      
-   ;     mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
-;    mov esi,edx ;AHORA ESI TIENE LA DIRECCION DEL PUNTERO NODO 
-    ;    add esi,8 ; AUMENTAMOS 8 A EDX PARA ACCEDER A LA DIRECCION DEL PUNTERO DEL NODO IZQ
-   ; mov esi,[esi]
-     
-    ;  mov ebx,0
-    ;  cmp [esi],ebx
-    ;  je asignar1
-      
-  
-    jmp finalizar
-   ; asignar1:
-   ; mov [esi],eax
-   ; jmp finalizar
-       
-   finalizar:
-             
-        mov     [EBP-4], EAX
-        mov     ESP, EBP
-        pop     EBP
-        ret  
+           push dword msg_parteIZQ
+           call _printf
+           add   ESP, 4
+           mov esi, [EBP+8] ;DIRECCION DEL PUNTERO
+           add esi,8 ; AUMENTAMOS 8 A EDX PARA ACCEDER A LA DIRECCION DEL PUNTERO DEL NODO IZQ
+           mov edx,esi
+           mov edx,[edx]
+           cmp edx,0
+           je insertarElNodo
+           mov esi,edx  
+
+   insertarElNodo:
+          push ebx
+          push esi
+          call _insertaNodo
+          pop esi
+          pop ebx
+          jmp finalizar
+   
+   finalizar:   
+           mov     [EBP-4], EAX
+           mov     ESP, EBP
+           pop     EBP
+           ret  
--- _imprimir.asm
+++ _imprimir.asm
@@ -1,9 +1,9 @@
 section .data
 msg1:   db     `\n*****EN ASSEMBLER - FUNCION IMPRIMIR\nDIR.PUNTERO = %p\nDIR.NODO = %p VAL.NODO = %d\nD.pD = %p V.pD = %p \nD.pI = %p V.pI = %p\n`, 10, 0
-msg2:   db     `DIRECCION PUNTERO EN FUNCION IMPRIMIR = %P \n`, 10, 0
 section .text
 extern _printf
 global _imprimir
+
 _imprimir:
     push EBP
     mov EBP, ESP
@@ -15,15 +15,15 @@ _imprimir:
     add     edx,8           
     push    dword edx       ;MUESTRO DIRECCION PUNTERO DERECHO
     push    dword  [eax+4]  ;MUESTRO VALOR PUNTERO IZQUIERDO
-        add     eax,4
-        push    dword eax       ;MUESTRO DIRECCION PUNTERO IZQUIERDO
-        sub     eax,4
-        push    dword  [EAX]    ;MUESTRO VALOR DEL NODO
-        push    dword eax       ;MUESTRO DIRECCION DEL VALOR DEL NODO
-        push    dword ebx       ;MUESTRO DIRECCION DEL PUNTERO
-        push    dword msg1
-        call    _printf
-        add ESP, 32  
-        mov     ESP, EBP
-        pop     EBP
-        ret 
+    add     eax,4
+    push    dword eax       ;MUESTRO DIRECCION PUNTERO IZQUIERDO
+    sub     eax,4
+    push    dword  [EAX]    ;MUESTRO VALOR DEL NODO
+    push    dword eax       ;MUESTRO DIRECCION DEL VALOR DEL NODO
+    push    dword ebx       ;MUESTRO DIRECCION DEL PUNTERO
+    push    dword msg1
+    call    _printf
+    add ESP, 32  
+    mov     ESP, EBP
+    pop     EBP
+    ret 
